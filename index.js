ed259a615450de235ee7de6c6333437a7813eee5
arjunmehra <arjunmehra2403@gmail.com>
2025-06-05 11:49:45 +0530

    Done

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-const input = document.getElementById("input");
-const uls = document.getElementById("uls")
-
-function funClick() {
-    if (input.value === "") {
-        alert("Please Enter Some-Thing!!!");
-    }
-    else {
-        let li1 = document.createElement("li");
-        li1.innerText = input.value;
-        uls.appendChild(li1)
-        let remove = document.createElement("button");
-        remove.innerText = "remove";
-        remove.classList.add("remove_btn");
-        li1.appendChild(remove);
-        remove.addEventListener("click", () => {
-            li1.remove();
-        })
-
-        let edit = document.createElement("button");
-        edit.innerText = "edit";
-        edit.classList.add("edit_btn");
-        li1.appendChild(edit);
-        edit.addEventListener("click", () => {
-            let new_data = prompt("Enter New Task Here!!!!");
-            console.log(new_data);
-            if (new_data && new_data.trim() !== "") {
-                li1.firstChild.textContent = new_data; 
-            }
-        })
-    }
-    input.value = "";
+// function statement:- Function statement is basically the way of creating function is known as function statement and function statement is also known as function decleartion
+function xyz()
+{
+    console.log("Hello1");
 }
+xyz();
+
+// function Expression:- Function expression is basically the ability of a function to be use as an value is known as Function Expression.
+var x=function (){
+    console.log("Hello2")
+}
+x();
+
+//Difference between function statement and function expression is function statement a.k.a are hoisted but function expression are not hoisted
+
+// Anynomous function:- Anynomous function is a function without a name is Anynomous function and Anynomous function are used at that place where a function is treated like a varabile
+
+var func=function(){  //This is Anynomous func
+    console.log("Hello3");  
+}
+func();
+
+// Name Function Expression:-
+
+var func2=function xc(){
+    console.log("Hello4");
+}
+func2();
+
+//First Class Function :- First class function is basically the ability of a function to be use as an value and pass them into another function and can return from another function so the ability is known as first class function.
